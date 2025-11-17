7f99ad61eb9bf6524dbad752f3deac97b421780b
Jeon Jong Heon <122503945+skufmid@users.noreply.github.com>
2025-11-17 21:35:35 +0900

    실습

--- Source/Player/SoftRenderer2D.cpp
+++ Source/Player/SoftRenderer2D.cpp
@@ -70,6 +70,24 @@ void SoftRenderer::Update2D(float InDeltaSeconds)
 	static float currentDegree = 0.f;
 	static float rotateSpeed = 180.f;
 	static float distance = 250.f;
+	static std::random_device rd;
+	static std::mt19937 mt(rd());
+	static std::uniform_real_distribution<float> randomY(-200.f, 200.f);
+
+	elapsedTime = Math::Clamp(elapsedTime + InDeltaSeconds, 0.f, duration);
+	if (elapsedTime == duration)
+	{
+		lineStart = Vector2(-400.f, randomY(mt));
+		lineEnd = Vector2(400.f, randomY(mt));
+		elapsedTime = 0.f;
+	}
+
+	// 점의 위치 결정
+	currentDegree = Math::FMod(currentDegree + rotateSpeed * InDeltaSeconds, 360.f);
+	float sin = 0.f, cos = 0.f;
+	Math::GetSinCos(sin, cos, currentDegree);
+	point = Vector2(cos, sin) * distance;
+
 }
 
 // 렌더링 로직을 담당하는 함수
@@ -109,6 +127,22 @@ void SoftRenderer::Render2D()
 	// 투영할 라인 그리기
 	r.DrawLine(lineStart, lineEnd, LinearColor::Black);
 	r.DrawLine(lineStart, point, LinearColor::Red);
+
+	// 투영된 위치와 거리 계산
+	Vector2 unitV = (lineEnd - lineStart).GetNormalize();
+	Vector2 u = point - lineStart;
+	Vector2 projV = unitV * (u.Dot(unitV));
+	Vector2 projectedPoint = lineStart + projV;
+	float distance = (projectedPoint - point).Size();
+
+	// 투영된 점 그리기
+	for (auto const& v : circle)
+	{
+		r.DrawPoint(v + projectedPoint, LinearColor::Magenta);
+	}
+
+	// 투영 라인 그리기
+	r.DrawLine(projectedPoint, point, LinearColor::Gray);
 }
 
 // 메시를 그리는 함수
